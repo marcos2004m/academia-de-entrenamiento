8c798c9929b195f0aea36051048cffc532ca7aeb
marcos2004m <109930600+marcos2004m@users.noreply.github.com>
2022-09-04 22:05:50 -0300

    Add files via upload

--- academia de entrenamiento/js/questions.js
+++ academia de entrenamiento/js/questions.js
@@ -5,7 +5,11 @@ titleQuestions.forEach(questions =>{
 questions.addEventListener('click', ()=>{
     let height =0;
     let answer = questions.nextElementSibling;
-    let addpadding = questions.parentElement.
+    let addpadding = questions.parentElement.parentElement;
+
+
+    addpadding.classList.toggle('questions__padding--add');
+
     questions.children[0].classList.toggle('questions__arrow--rotate');
 
     if(answer.clientHeight === 0){
