beefc88b9449e928fa9da377f54e77889e1aa0d3
marcos2004m <109930600+marcos2004m@users.noreply.github.com>
2022-09-09 21:12:39 -0300

    Add files via upload

--- academia de entrenamiento/js/menu.js
+++ academia de entrenamiento/js/menu.js
@@ -1,15 +1,21 @@
 (function(){
-    const openButton = document.querySelector('.nav__menu')
-    const menu = document.querySelector('.nav__link')
-   const  closeMenu = document.querySelector ('.nav__close')
+    const titleQuestions = [...document.querySelectorAll('.questions__title')];
+    console.log(titleQuestions)
 
-    openButton.addEventListener ('click' , ()=>{
-    menu.classList.add('nav__link--show');
+    titleQuestions.forEach(question =>{
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
 
+            addPadding.classList.toggle('questions__padding--add');
+            question.children[0].classList.toggle('questions__arrow--rotate');
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`;
+        });
     });
-closeMenu.addEventListener( 'click', ()=>{
-    menu.classList.remove ('nav__link--show');
-})
-
 })();
-
